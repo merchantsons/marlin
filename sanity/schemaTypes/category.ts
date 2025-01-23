757b1f90058c4d9291e323e8cf5082f32de63afb
merchantsons <160190585+merchantsons@users.noreply.github.com>
2025-01-24 01:08:53 +0500

    Add files via upload

---
+++ sanity/schemaTypes/category.ts
@@ -0,0 +1,17 @@
+export default {
+    name: 'category',
+    title: 'Categories',
+    type: 'document',
+    fields: [
+      {
+        name: 'id',
+        title: 'Category ID #',
+        type: 'string'
+      },
+      {
+        name: 'category',
+        title: 'Category',
+        type: 'string'
+      }
+    ]
+  };
--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -4,7 +4,8 @@ import colors from './colors'
 import sizes from './sizes'
 import newsletter from './newsletter'
 import users from './users'
+import category from './category'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [shopco,colors,sizes,newsletter,users],
+  types: [shopco,colors,sizes,newsletter,users,category],
 }
